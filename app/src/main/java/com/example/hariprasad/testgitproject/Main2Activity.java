306c0d42abac5cf93bc8072d218a38c5c9160d65
Harirexzeon <hariprasadragupathy@gmail.com>
2015-08-31 15:24:07 +0530

    test 123

--- app/src/main/java/com/example/hariprasad/testgitproject/Main2Activity.java
+++ app/src/main/java/com/example/hariprasad/testgitproject/Main2Activity.java
@@ -12,6 +12,7 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         //sds
+        //commit 2
     }
 
     @Override
