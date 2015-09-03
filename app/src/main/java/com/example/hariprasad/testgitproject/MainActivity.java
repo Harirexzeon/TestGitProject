a743d475e3fc15884b1ad2f340c8322113b6663f
Gangadhar <gangadhar.g@mmadapps.com>
2015-09-03 13:22:30 +0530

    gan c 1

--- app/src/main/java/com/example/hariprasad/testgitproject/MainActivity.java
+++ app/src/main/java/com/example/hariprasad/testgitproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hariprasad.testgitproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         //Hari Master
         //saurav sani
         //aman
+        Log.d("dfhdh","hdjhgkjah");
     }
 
     @Override
